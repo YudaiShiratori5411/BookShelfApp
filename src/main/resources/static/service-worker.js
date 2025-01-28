81890a5d385d670f12f49f2604f74928aaf769b4
YudaiShiratori5411 <bainiaxiongda620@gmail.com>
2025-01-28 18:00:57 +0900

    CSSのコードをより細分化しました。

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -1,28 +1,42 @@
 const CACHE_NAME = 'bookshelf-v1';
 const urlsToCache = [
-  '/',
-  '/login',
-  '/css/bookshelf.css',
-  '/css/auth-tabs.css',
-  '/css/orientation.css',
-  '/css/reading-count.css',
-  '/css/dropdown.css',
-  '/css/shelf-divider.css',
-  '/css/search-bar.css',
-  '/js/search-bar.js',
-  '/js/image-upload.js',
-  '/js/scanner/BookFormScanner.js',
-  '/js/reading-count.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+    '/',
+    '/login',
+    '/css/components/book-card.css',
+    '/css/components/cover-image.css',
+    '/css/components/forms.css',
+    '/css/components/layout.css',
+    '/css/components/navigation.css',
+    '/css/components/shelf.css',
+    '/css/components/sorting.css',
+    '/css/auth-tabs.css',
+    '/css/dropdown.css',
+    '/css/orientation.css',
+    '/css/reading-count.css',
+    '/css/search-bar.css',
+    '/css/shelf-divider.css',
+    '/js/search-bar.js',
+    '/js/image-upload.js',
+    '/js/scanner/BookFormScanner.js',
+    '/js/reading-count.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
 // インストール時の処理
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opening cache...');
+                const cachePromises = urlsToCache.map(url => {
+                    return cache.add(url).catch(err => {
+                        console.error('Error caching', url, err);
+                    });
+                });
+                return Promise.all(cachePromises);
+            })
+    );
 });
 
 // フェッチ時の処理
